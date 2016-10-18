cbd6c19c3b541ac2edaca1a9d03f0339bddbd481
Jonas Möller <moeller-jonas@web.de>
2016-10-18 17:02:31 +0200

    Add mockup-data to railroad-service

--- app/elements/railroad/railroad.service.ts
+++ app/elements/railroad/railroad.service.ts
@@ -1,22 +1,79 @@
-import { Injectable } from '@angular/core';
+/*
+ * Copyright Siemens AG, 2016
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-export class Station {
-	constructor(private name: string) { }
-}
+/**
+ *
+ * @author Jonas Möller
+ */
+import { Injectable } from '@angular/core';
 
 export interface Halt {
 	station: Station,
 	time: Date
 }
 
+export type Station = string;
 type Track = Halt[];
 export type Railroad = Track[];
 
 const stations: Station[] = [
-	new Station("A"), new Station("B"), new Station("C")
+	"A", "B", "C"
 ];
 
 const railroad: Railroad = [
+	[{
+		station: stations[0],
+		time: new Date(0, 0, 0, 10, 0, 0)
+	},
+	{
+		station: stations[0],
+		time: new Date(0, 0, 0, 10, 10, 0)
+	},
+	{
+		station: stations[1],
+		time: new Date(0, 0, 0, 11, 0, 0)
+	},
+	{
+		station: stations[1],
+		time: new Date(0, 0, 0, 11, 5, 0)
+	},
+	{
+		station: stations[2],
+		time: new Date(0, 0, 0, 11, 30, 0)
+	}],
+	[{
+		station: stations[0],
+		time: new Date(0, 0, 0, 10, 0, 0)
+	},
+	{
+		station: stations[0],
+		time: new Date(0, 0, 0, 10, 10, 0)
+	},
+	{
+		station: stations[1],
+		time: new Date(0, 0, 0, 11, 0, 0)
+	},
+	{
+		station: stations[1],
+		time: new Date(0, 0, 0, 11, 5, 0)
+	},
+	{
+		station: stations[2],
+		time: new Date(0, 0, 0, 11, 30, 0)
+	}],
 	[{
 		station: stations[0],
 		time: new Date(0, 0, 0, 10, 0, 0)
